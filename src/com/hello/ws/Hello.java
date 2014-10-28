8cc56c671c7e28d020e115b01b351c43237fbd02
farid <farid696@yahoo.com>
2014-10-28 13:33:39 -0700

    first commit of helloworld

---
+++ src/com/hello/ws/Hello.java
@@ -0,0 +1,16 @@
+package com.hello.ws;
+
+
+
+
+public class Hello {
+    private String message = new String("Hello, ");
+
+    public void Hello() {
+    }
+
+
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
+}
